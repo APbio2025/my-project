e79196ec8b9a5b844f0a56a970e7523339dee320
APbio2025 <dwyanemyers280@gmail.com>
2025-03-14 10:40:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/api/message", (req, res) => {
 
 // Serve Frontend
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.htm"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
